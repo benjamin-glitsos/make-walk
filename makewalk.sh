07397ace6287f5bf978f3a643e6eb15d76038764
Benjamin Glitsos <bengyup@gmail.com>
2021-01-10 12:31:06 +1100

    Some code formatting using comments

--- makewalk.sh
+++ makewalk.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
+# Settings --------------------
+
 readonly separator="-"
+
+# Utilities --------------------
 
 function join_by_separator() {
     local IFS=$separator; echo "$*";
 }
+
+function echo_and_run() {
+    echo "$*" ; "$@" ;
+}
+
+# Main --------------------
 
 readonly fullpath=`join_by_separator "$@"`
 readonly filename=`basename $fullpath`
