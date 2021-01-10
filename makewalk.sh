8c1fd69cf69bd30319732b086906097fffb76fea
Benjamin Glitsos <bengyup@gmail.com>
2021-01-10 12:51:11 +1100

    Improved the shebang

--- makewalk.sh
+++ makewalk.sh
@@ -1,24 +1,29 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-# Settings --------------------
+function makewalk() {
+    # Settings --------------------
 
-readonly separator="-"
+    readonly separator="-"
 
-# Utilities --------------------
+    # Utilities --------------------
 
-function join_by_separator() {
-    local IFS=$separator; echo "$*";
+    function join_by_separator() {
+        local IFS=$separator; echo "$*";
+    }
+    readonly -f join_by_separator &>/dev/null
+
+    function run_and_echo() {
+        echo "$*" ; "$@" ;
+    }
+    readonly -f run_and_echo &>/dev/null
+
+    # Main --------------------
+
+    readonly fullpath=`join_by_separator "$@"`
+    readonly filename=`basename $fullpath`
+    readonly dirpath=`dirname $fullpath`
+
+    run_and_echo `mkdir -p $dirpath`
+    run_and_echo `cd $dirpath`
 }
-
-function echo_and_run() {
-    echo "$*" ; "$@" ;
-}
-
-# Main --------------------
-
-readonly fullpath=`join_by_separator "$@"`
-readonly filename=`basename $fullpath`
-readonly dirpath=`dirname $fullpath`
-
-echo $filename
-echo $dirpath
+readonly -f makewalk &>/dev/null
