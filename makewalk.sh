93b89d96ad896ee4c774233f7fdd5d7f2d137768
Benjamin Glitsos <bengyup@gmail.com>
2021-01-10 13:54:55 +1100

    Started creating the logic that handles edge cases and wrote some todos

--- makewalk.sh
+++ makewalk.sh
@@ -6,7 +6,7 @@ declare -rf makewalk() {
     declare -r separator="-"
     declare -r opener="vimer"
 
-    # Colous --------------------
+    # Colors --------------------
 
     declare -r blue="\033[0;34m"
     declare -r purple="\033[0;35m"
@@ -29,8 +29,19 @@ declare -rf makewalk() {
     declare -r filename=`basename $fullpath`
     declare -r dirpath=`dirname $fullpath`
 
-    echo_and_run $blue "mkdir -p $dirpath"
-    echo_and_run $blue "cd $dirpath"
-    echo_and_run $purple "touch $filename"
-    echo_and_run $purple "$opener $filename"
+    # TODO: remove these
+    echo $filename
+    echo $dirpath
+
+    # TODO: check if path ends in slash and if so, append the filename to the dirpath and replace filename with "."
+    # TODO: split filenames by comma then use a for loop to echo_and_run a touch and opener command for each
+    if [ $dirpath != "." ]; then # TODO: make boolean function that does this check
+        echo_and_run $blue "mkdir -p $dirpath"
+        echo_and_run $blue "cd $dirpath"
+    fi
+
+    if [ $filename != "." ]; then
+        echo_and_run $purple "touch $filename"
+        echo_and_run $purple "$opener $filename"
+    fi
 }
