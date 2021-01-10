5d618c0565d1f13398bc6f1a703ee7e7e3061d22
Benjamin Glitsos <bengyup@gmail.com>
2021-01-10 16:32:45 +1100

    All conditionals in the Main section now working

--- makewalk.sh
+++ makewalk.sh
@@ -6,19 +6,27 @@ declare -rf makewalk() {
     declare -r separator="-"
     declare -r opener="xdg-open"
 
-    # Colors --------------------
+    # Constants --------------------
 
+    declare -r empty_path="."
     declare -r purple="\033[0;35m"
     declare -r nocolor="\033[0m"
 
     # Utilities --------------------
 
     declare -rf join_by_separator() {
-        declare IFS=$separator; echo "$*";
+        declare IFS=$separator; echo $*;
     }
 
     declare -rf not_empty_path() {
-        if [[ "$*" != "." ]]; then
+        if [[ $* != $empty_path ]]; then
+            return
+        fi
+        false
+    }
+
+    declare -rf does_end_with_slash() {
+        if [[ $* =~ "/$" ]]; then
             return
         fi
         false
@@ -31,14 +39,15 @@ declare -rf makewalk() {
     # Main --------------------
 
     declare -r fullpath=`join_by_separator "$@"`
-    declare -r filename=`basename $fullpath`
-    declare -r dirpath=`dirname $fullpath`
 
-    # TODO: remove these
-    echo $filename
-    echo $dirpath
+    if does_end_with_slash $fullpath; then
+        declare -r filename=$empty_path
+        declare -r dirpath=$fullpath
+    else
+        declare -r filename=`basename $fullpath`
+        declare -r dirpath=`dirname $fullpath`
+    fi
 
-    # TODO: check if path ends in slash and if so, append the filename to the dirpath and replace filename with "."
     # TODO: split filenames by comma then use a for loop to echo_and_run a touch and opener command for each
     if not_empty_path $dirpath; then
         echo_and_run "mkdir -p $dirpath && cd $dirpath"
