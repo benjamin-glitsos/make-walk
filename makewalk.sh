06c7359dcf95299d8b741445f2813f40bedf0b2f
Benjamin Glitsos <bengyup@gmail.com>
2021-01-10 17:06:53 +1100

    Split on comma and loop over them now working plus slightly improved console colours

--- makewalk.sh
+++ makewalk.sh
@@ -4,19 +4,17 @@ declare -rf makewalk() {
     # Settings --------------------
 
     declare -r separator="-"
+    declare -r delimiter=","
     declare -r opener="xdg-open"
 
     # Constants --------------------
 
     declare -r empty_path="."
     declare -r purple="\033[0;35m"
+    declare -r cyan="\033[0;36m"
     declare -r nocolor="\033[0m"
 
     # Utilities --------------------
-
-    declare -rf join_by_separator() {
-        declare IFS=$separator; echo $*;
-    }
 
     declare -rf not_empty_path() {
         if [[ $* != $empty_path ]]; then
@@ -32,8 +30,16 @@ declare -rf makewalk() {
         false
     }
 
+    declare -rf join_by_separator() {
+        declare IFS=$separator; echo $*;
+    }
+
+    declare -rf split_by_comma() {
+        echo $* | tr $delimiter "\n";
+    }
+
     declare -rf echo_and_run() {
-        echo "\$$purple $* $nocolor"; eval $*;
+        echo "$cyan\$$nocolor$purple $* $nocolor"; eval $*;
     }
 
     # Main --------------------
@@ -41,19 +47,21 @@ declare -rf makewalk() {
     declare -r fullpath=`join_by_separator "$@"`
 
     if does_end_with_slash $fullpath; then
-        declare -r filename=$empty_path
+        declare -r filenames=$empty_path
         declare -r dirpath=$fullpath
     else
-        declare -r filename=`basename $fullpath`
+        declare -r filenames=`basename $fullpath`
         declare -r dirpath=`dirname $fullpath`
     fi
 
-    # TODO: split filenames by comma then use a for loop to echo_and_run a touch and opener command for each
     if not_empty_path $dirpath; then
         echo_and_run "mkdir -p $dirpath && cd $dirpath"
     fi
 
-    if not_empty_path $filename; then
-        echo_and_run "touch $filename && $opener $filename"
+    if not_empty_path $filenames; then
+        for filename in `split_by_comma $filenames`
+        do
+            echo_and_run "touch $filename && $opener $filename"
+        done
     fi
 }
