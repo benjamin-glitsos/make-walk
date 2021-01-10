caa07cc1a81591fbe45fa15396e0290ffda7a3f7
Benjamin Glitsos <bengyup@gmail.com>
2021-01-10 13:15:19 +1100

    Some bash type-safety is working

--- makewalk.sh
+++ makewalk.sh
@@ -1,32 +1,29 @@
 #!/usr/bin/env bash
 
-function makewalk() {
+declare -rf makewalk() {
     # Settings --------------------
 
-    readonly separator="-"
-    readonly opener="vimer"
+    declare -r separator="-"
+    declare -r opener="vimer"
 
     # Utilities --------------------
 
-    function join_by_separator() {
-        local IFS=$separator; echo "$*";
+    declare -rf join_by_separator() {
+        declare IFS=$separator; echo "$*";
     }
-    readonly -f join_by_separator &>/dev/null
 
-    function echo_and_run() {
+    declare -rf echo_and_run() {
         echo "\$ $*"; eval $*;
     }
-    readonly -f echo_and_run &>/dev/null
 
     # Main --------------------
 
-    readonly fullpath=`join_by_separator "$@"`
-    readonly filename=`basename $fullpath`
-    readonly dirpath=`dirname $fullpath`
+    declare -r fullpath=`join_by_separator "$@"`
+    declare -r filename=`basename $fullpath`
+    declare -r dirpath=`dirname $fullpath`
 
     echo_and_run "mkdir -p $dirpath"
     echo_and_run "cd $dirpath"
     echo_and_run "touch $filename"
     echo_and_run "$opener $filename"
 }
-readonly -f makewalk &>/dev/null
